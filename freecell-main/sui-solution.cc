215dab2fa2c6651d6c7161ae64ea845103baee37
jakubKuznik <kuznja16@gmail.com>
2023-10-11 18:47:15 +0200

    DFS BFS - final

--- freecell-main/sui-solution.cc
+++ freecell-main/sui-solution.cc
@@ -1,11 +1,11 @@
 #include "search-strategies.h"
-#include <queue>
+#include "mem_watch.h"
+#include "memusage.h"
 
+#include <queue>
 #include <algorithm>
 #include <set>
 
-#include "mem_watch.h"
-#include "memusage.h"
 
 #define BFS_MEM_LIMIT_BYTES 50000000 //50MB
 
@@ -21,25 +21,38 @@ std::vector<SearchAction> BreadthFirstSearch::solve(const SearchState &init_stat
 	// Finall solution vector  
 	std::vector<SearchAction> solution;
 
-	// init first state 
-	SearchState working_state(init_state);
-	
 	// vector with currently processed actions 	
 	std::vector<SearchAction> current_actions;
 
 	// CLOSED 
 	std::set<SearchState> closed;
 
-	// look ahead buffer 	
+	// 	helping variable 
+	std::vector<SearchAction> temp_actions; 
+
+	// look ahead buffer for optimalization
 	std::vector<SearchAction> potentional_solution; 
+	bool found_potential = false;
+	long unsigned int height = 0; 
+	
+	// init open 
+	SearchState working_state(init_state);
+	if (working_state.isFinal()){
+		return {};
+	}
+	// first expand 
+	std::vector<SearchAction> expand_actions = working_state.actions();
+	for (const SearchAction& act : expand_actions) {
+		std::vector<SearchAction> action;
+		action.push_back(act);
+		open.push(action);
+	}
 
-	for (size_t path= 0; ; path++) {
+	// ends if there is no solution 
+	while (!open.empty()) {
 
-		SearchState working_state(init_state);  
-		
-		// ... GET CURRENT STATE
-		// if que is not empty go to the state otherwise it is first try 
-		// This is to way to get to currently procesed node 
+		SearchState working_state(init_state);
+		// Get to currently procesed node 
 		if (!open.empty()){
 			current_actions = open.front();
 			open.pop();
@@ -47,48 +60,47 @@ std::vector<SearchAction> BreadthFirstSearch::solve(const SearchState &init_stat
 				working_state = action.execute(working_state);
 			}
 		}
-		// There is no solution
-		else if (open.empty() && path > 0){ 
-			return {};
+		// Check if currently expanded node is already a solution	
+		if (working_state.isFinal()){
+			return current_actions;
+		}
+		// optimalization 
+		if (current_actions.size() > height){
+			if (found_potential == true){
+				return potentional_solution;
+			}
+			found_potential = false;
+			height++;
 		}
 		
 		// ... GENERATE NEXT STATES and expand them 
-		std::vector<SearchAction> actions = working_state.actions();		
-		for (const SearchAction& action: actions) {
-
-			// story whole new path to the queue 
+		expand_actions = working_state.actions();		
+		for (const SearchAction& action: expand_actions) {
 			//  current actions (S,A,B) + actuall == (S,A,B,C)
-			potentional_solution = current_actions; 
-			potentional_solution.push_back(action);
+			temp_actions= current_actions; 
+			temp_actions.push_back(action);
 			
 			SearchState temp_state(init_state); 
-			for (const SearchAction& action : potentional_solution) {
+			for (const SearchAction& action : temp_actions) {
 				temp_state = action.execute(temp_state);
 			}
-			
 			// IF state is already in closed dont expand 
 			if (closed.find(temp_state) == closed.end()) {
-				
-				// Check if currently expanded node is already a solution	
-				if (temp_state.isFinal()){
-					printf("Closed: %ld Open: %ld",closed.size(), open.size() );
-					printf(" Finnal mem usage %ld KB", (getCurrentRSS()/1000)); //TODO REMOVE
-					printf(" SOLUTION DEPTH %ld\n", potentional_solution.size()); //TODO REMOVE
-
-					return potentional_solution;
+				// optimalization - if currently expanded node is already a solution	
+				if (found_potential == false){
+					if (temp_state.isFinal()){
+						potentional_solution = temp_actions;
+						found_potential = true;
+					}
 				}
-				
 				// store action vector to queue
 				closed.insert(temp_state);
-				open.push(potentional_solution);
+				open.push(temp_actions);
 			}
-
 		}
 
 		// mem test // 50MB
 		if ((size_t)getCurrentRSS() > mem_limit_ - BFS_MEM_LIMIT_BYTES) {
-			printf("Closed: %ld Open: %ld ",closed.size(), open.size() );
-			printf("MEM crash %ld KB\n", (getCurrentRSS()/1000)); //TODO REMOVE 
 			return {};
 		}
 	}
@@ -98,63 +110,60 @@ std::vector<SearchAction> BreadthFirstSearch::solve(const SearchState &init_stat
 
 std::vector<SearchAction> DepthFirstSearch::solve(const SearchState &init_state) {
 
+	// OPEN: queue with next states 
+	//            S 
+	//       A   B  C  D
+	// [(S,D),(S,C),(S,B),(S,A)] 
 	std::vector<std::vector<SearchAction>> open;
+
+	// One vector of actions that is currently being processed 
 	std::vector<SearchAction> current_actions;
+
+	// Temp variable for expanding  
 	std::vector<SearchAction> actions;
-	std::vector<std::vector<SearchAction>> init_actions;
-	
+
 	// init open 
 	SearchState working_state(init_state);
 	actions = working_state.actions();
 	if (working_state.isFinal()){
 		return {};
 	}
-
+	// Expand first state 
 	for (const SearchAction& act : actions) {
 		std::vector<SearchAction> action;
 		action.push_back(act);
-		init_actions.push_back(action);
+		open.push_back(action);
 	}
+
+	while(!open.empty()){
+		SearchState working_state(init_state); // set to first state 
 	
-	size_t depth = 1;
-	printf("==================== BEGIN =================\n");
-	for (;depth <= DepthFirstSearch::depth_limit_; depth++) {
-		printf("Depth: %ld open size: %ld closed size: NONE mem: %ld KB\n",depth, open.size(),getCurrentRSS()/1000);
-		open = init_actions;
+		// stack approach 
+		std::vector<SearchAction> current_actions = open.back(); 
+		open.pop_back();
 
-		while(!open.empty()){
-			SearchState temp_state(init_state);
-			std::vector<SearchAction> current_actions = open.back(); // stack approach 
-			open.pop_back();
-
-			// this approach si slower but much more memory efficent 	
-			for (const SearchAction& action : current_actions) {
-				temp_state = action.execute(temp_state);
-			}
-			if (temp_state.isFinal()){
-				printf("=========  solution found DEPTH: %ld ============= \n\n", current_actions.size());
-				return current_actions;
-			}
-			if (current_actions.size() < depth){
-				actions = temp_state.actions();
-				for (const SearchAction& act : actions) {
-					std::vector<SearchAction> new_actions = current_actions;
-					new_actions.push_back(act);
-					open.push_back(new_actions);
-				}
-			}
-			// mem test // 50MB
-			if ((size_t)getCurrentRSS() > mem_limit_ - BFS_MEM_LIMIT_BYTES) {
-				printf("closed: NONE Open: %ld\n", open.size() );
-				printf("MEM crash %ld KB\n", (getCurrentRSS()/1000)); //TODO REMOVE 
-				return {};
+		// this approach si slower but much more memory efficent 	
+		for (const SearchAction& action : current_actions) {
+			working_state = action.execute(working_state);
+		}
+		if (working_state.isFinal()){
+			return current_actions;
+		}
+		// THIS ESTABLISHES THE MAX HEIGHT 
+		if (current_actions.size() < (long unsigned int)DepthFirstSearch::depth_limit_){
+			actions = working_state.actions();
+			for (const SearchAction& act : actions) {
+				std::vector<SearchAction> new_actions = current_actions;
+				new_actions.push_back(act);
+				open.push_back(new_actions);
 			}
 		}
-		// clean and iterate 
-		std::vector<SearchAction>().swap(current_actions);
-	
+		// mem test // 50MB
+		if ((size_t)getCurrentRSS() > mem_limit_ - BFS_MEM_LIMIT_BYTES) {
+			return {};
+		}
 	}
-	printf("==================== Depth limit %ld ====================\n\n", depth-1);
+	
 	return {};
 }
 
@@ -164,7 +173,7 @@ double StudentHeuristic::distanceLowerBound(const GameState &state) const {
 		// the further we are the bigger the number is
 		
 		// Bigger the number -> bigger the penalisation 
-		double out = 0; 
+		//double out = 0; 
 
 		// Get current homeStack
 		// based on that find out what card should follow 
