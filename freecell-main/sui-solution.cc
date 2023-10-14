bb0c9c1750bacf77f5cbf442a25d42d6cb7e1542
svobodovaLucie <xsvobo1x@stud.fit.vutbr.cz>
2023-10-14 16:36:37 +0200

    A* - heuristic used, needs to be optimalized and checked

--- freecell-main/sui-solution.cc
+++ freecell-main/sui-solution.cc
@@ -74,11 +74,13 @@ std::vector<SearchAction> BreadthFirstSearch::solve(const SearchState &init_stat
 		}
 		// Check if currently expanded node is already a solution	
 		if (working_state.isFinal()){
+			std::cout << std::endl << "Real solution returned in depth: " << current_actions.size() << "\nfinal_state:\n" << working_state << std::endl;
 			return current_actions;
 		}
 		// optimalization 
 		if (current_actions.size() > depth){
 			if (found_potential == true){
+				std::cout << "Potential solution found in depth: " << current_actions.size() << std::endl;
 				return potentional_solution;
 			}
 			found_potential = false;
@@ -257,8 +259,6 @@ std::vector<std::string> find_next_cards_values(const GameState &state) {
 
 }
 
-
-
 double StudentHeuristic::distanceLowerBound(const GameState &state) const {
 
 	// compute the values of next cards on home stacks
@@ -273,11 +273,6 @@ double StudentHeuristic::distanceLowerBound(const GameState &state) const {
 		// std::cout << next_cards[i] << ", ";
 		next_cards_string << next_cards[i];
 	}
-	// std::cout << std::endl;
-
-	// std::cout << "Next cards string: " << next_cards_string.str() << std::endl;
-
-
 
 	// go through the work stacks and find the cards
 		std::stringstream ss;
@@ -315,366 +310,9 @@ double StudentHeuristic::distanceLowerBound(const GameState &state) const {
 		}
 
 		// std::cout << "heuristic: " << result << std::endl;
-		result = 0;
-		return result;
+		// result = 0;
+		return result/2.0;
 }
-/*
-		for (WorkStack& stack : work_stacks) {
-    	while (true) {
-			  std::optional<Card> card = stack.getCard();
-        if (!card.has_value()) {
-            break; // Exit the loop when there are no more cards
-        }
-        // std::cout << card.value() << "";
-				// std::cout << "(" << king_value << ")";
-				ss << card.value();
-				std::string card_string = ss.str();
-				// printf(" [%c,%c]", card_string[card_string.length() - 2], card_string[card_string.length() - 1]);
-				std::stringstream card_value_stream;
-				card_value_stream << card_string[card_string.length() - 2];
-				
-				// save the card value as a number
-				unsigned card_value;
-				if (card_value_stream.str() == "K") {
-					card_value = 13;
-				} else if (card_value_stream.str() == "Q") {
-					card_value = 12;
-				} else if (card_value_stream.str() == "J") {
-					card_value = 11;  
-				} else {
-					card_value_stream >> card_value;
-				}
-				// printf("[card_value: %u]", card_value);
-				// std::cout << ", ";
-
-				// declaration for switch
-				std::stringstream hcard;
-				std::string home_card_string;
-				std::stringstream home_card_value_stream;
-				double home_card_value = 0.0;	// default - empty home stack
-				// find the right home stack
-				switch (card_string[card_string.length() - 1])
-				{
-				case 's':
-					// save the home card value as a number
-					if (home_stacks[0].topCard().has_value()) {
-						hcard << home_stacks[0].topCard().value();	
-					} 
-					break;
-				case 'h':
-					// save the home card value as a number
-					if (home_stacks[1].topCard().has_value()) {
-						hcard << home_stacks[0].topCard().value();	
-					} 
-					break;
-				case 'd':
-					// save the home card value as a number
-					if (home_stacks[2].topCard().has_value()) {
-						hcard << home_stacks[0].topCard().value();	
-					} 
-					break;
-				case 'c':
-					// save the home card value as a number
-					if (home_stacks[3].topCard().has_value()) {
-						hcard << home_stacks[0].topCard().value();	
-					} 
-					break;
-				default:
-					break;
-				}
-				// asign the value of the card on work stack
-				home_card_string = hcard.str();
-				if (home_card_string[1] == 'K') {
-					home_card_value = 13.0;
-				} else if (home_card_string[1] == 'Q') {
-					home_card_value = 12.0;
-				} else if (home_card_string[1] == 'J') {
-					home_card_value = 11.0;  
-				} else {
-					home_card_value_stream << home_card_string[1];
-					home_card_value_stream >> home_card_value;
-				}
-				// return: 13 - diff
-				result = 13 - (card_value - home_card_value);
-   	 	}
-		 	// todo move cards back in order if there is no actuall copy 
-			// std::cout << std::endl;
-		}
-		*/
-///////////////////////////////////////////////////////////
-
-
-	// // TODO	
-	// // create vector for the current state -> does not have to be in each heuristic, but before calling the heuristic
-	// std::vector<std::string> next_cards = {"0", "0", "0", "0"};	// h,d,s,c
-	// // std::vector<int> next_cards_numbers = {0,0,0,0};	// h,d,s,c
-
-	// // find out the next cards values
-	
-	// return 0;
-	// std::array<HomeDestination, nb_homes> home_stacks;
-	// // std::cout << "kks: " << state.homes[3].topCard().value() << std::endl;
-	// // initialize the homes with 0
-	// // next_cards = {0h, 0d, 0s, 0c};
-	// for (int i = 0; i < 4; i++){
-	// 	if (state.homes[i].topCard().has_value()){
-	// 		std::stringstream card_value_str;
-	// 		card_value_str << state.homes[i].topCard().value();
-	// 		std::string without_last_char = card_value_str.str();	// for storing the number onlu
-	// 		without_last_char.pop_back();
-	// 		switch (card_value_str.str().back()) {
-	// 			case 'h':				
-	// 				next_cards[0] = without_last_char;
-	// 				break;
-	// 			case 'd':				
-	// 				next_cards[1] = without_last_char;
-	// 				break;
-	// 			case 's':				
-	// 				next_cards[2] = without_last_char;
-	// 				break;
-	// 			default:
-	// 				next_cards[3] = without_last_char;
-	// 				break;
-	// 		}
-	// 		// next_cards[i] = card_value_str.str();
-	// 	}
-	// }
-	
-	// // next_cards = {0h,1d,2s,0c}	// example
-	// int val;
-	// for (int i = 0; i < 4; i++) {
-	// 		try {
-	// 			val = stoi(next_cards[i]);
-	// 			if (val == 11) // 10 -> J
-	// 				next_cards[i] = "J";
-	// 			else {
-	// 				val++;
-	// 				next_cards[i] = std::to_string(val);
-	// 			}
-	// 		} catch(const std::exception& e) {
-	// 			std::string x = next_cards[i];
-	// 			std::cout << "bef x: " << x << std::endl;
-	// 			x.pop_back();
-	// 			std::cout << "aft x: " << x << std::endl;
-	// 			if (x == "J") // J -> Q
-	// 				next_cards[i] = "Q";
-	// 			else	// Q, K -> K
-	// 				next_cards[i] = "K";
-	// 		}
-	// 		std::cout << "string: " << next_cards[i] << ", int: " << val << std::endl;
-	// 	}
-
-	// 	// add the h,d,c,s
-	// 	next_cards[0].append("h");
-	// 	next_cards[1].append("d");
-	// 	next_cards[2].append("s");
-	// 	next_cards[3].append("c");
-
-	// 	std::cout << "to be found in state: " << std::endl << state << std::endl;
-	// 	for (int i = 0; i < 4; i++) {
-	// 		std::cout << next_cards[i] << ", ";
-	// 	}
-	// 	std::cout << std::endl;
-		
-
-	// return 0;
-		// generate random double:
-		// const long max_rand = 1000000L;
-    // double lower_bound = 0;
-    // double upper_bound = 100;
-    // srandom(time(NULL));
- 
-    //   // Using random function to
-    //   // get random double value
-    // double random_double = lower_bound
-    //                        + (upper_bound - lower_bound)
-    //                              * (random() % max_rand)
-    //                              / max_rand;
- 
-    // // std::cout << "generated random: " << random_double << std::endl;
-		// // ooo += 0.001;
-		// // return random_double + (ooo);
-		// return 0;
-		
-		// std::stringstream ss;
-	// ss << state;
-  // std::string state_string = ss.str();
-	// return std::hash<std::string>{}(state_string);
-
-    // int cards_out_of_home = king_value * colors_list.size();
-    // for (const auto &home : state.homes) {
-    //     auto opt_top = home.topCard();
-    //     if (opt_top.has_value())
-    //         cards_out_of_home -= opt_top->value;
-    // }
-
-    // return cards_out_of_home;
-
-
-		// todo distance to next card that can be in home_dest  
-		// the further we are the bigger the number is
-		
-		// Bigger the number -> bigger the penalisation 
-		//double out = 0; 
-
-		// Get current homeStack
-		// based on that find out what card should follow 
-    // std::array<HomeDestination, nb_homes> home_stacks;
-		/*
-		std::stringstream home_cards_ss;
-		// std::cout << "kks: " << state.homes[3].topCard().value() << std::endl;
-		for (int i = 0; i < 4; i++){
-			if (state.homes[0].topCard().has_value()){
-      	std::cout << "home " << state.homes[i].topCard().value();
-				home_cards_ss << state.homes[i].topCard().value();
-			} else {
-				if (i == 0) {
-					home_cards_ss << "0h";
-				} else if (i == 1) {
-					home_cards_ss << "0d";
-				} else if (i == 2) {
-					home_cards_ss << "0s";
-				} else {
-					home_cards_ss << "0c";
-				}
-			}
-		}
-		std::cout << "home_cards: " << home_cards_ss.str() << std::endl;
-		std::string home_cards = home_cards_ss.str();
-		// return 0;
-
-		// std::stringstream ss;
-		std::string value_stream;
-		char value = 0;
-		std::cout << "home_cards before: " << home_cards << std::endl;
-
-		// create the string back
-		for (int i = 0; i < 7; i = i+2) {
-			value = 0;
-			value << home_cards[i]; // value of h/d/s/c
-			// value++;
-			// check J, Q, K
-			std::cout << value << std::endl;
-			if (value >= 10) {
-				std::cout << value << std::endl;
-				if (value == 10) 
-					// rovnou pricteme 1
-					home_cards[i] = 'J';
-				if (value == 11)
-					// rovnou home_cards.str()[i]
-					home_cards[i] = 'Q';
-				else if (home_cards[i])
-					// rovnou pricteme 1
-					home_cards[i] = 'K';
-				// else if (value == 13)
-					// K se uz nezvetsi
-					// value_stream[i] = 'K';
-			} else {
-				value++;
-				std::cout << "ins: " <<  value << std::endl;
-				home_cards[i] = std::to_string(value)[0];
-			}
-			// home_cards.str()[0] << value;
-		}
-		
-		std::cout << "home_cards to be found: " << home_cards << std::endl;
-
-
-		return 0;
-    
-		std::stringstream ss;
-		std::array<WorkStack, nb_stacks> work_stacks = state.stacks;
-		// Get throughout home_dest and count the penalisation 
-		double result = 0.0;
-		for (WorkStack& stack : work_stacks) {
-			// std::cout << "work stack " << i++ << ": ";
-    	while (true) {
-			  std::optional<Card> card = stack.getCard();
-        if (!card.has_value()) {
-            break; // Exit the loop when there are no more cards
-        }
-        // std::cout << card.value() << "";
-				// std::cout << "(" << king_value << ")";
-				ss << card.value();
-				std::string card_string = ss.str();
-				// printf(" [%c,%c]", card_string[card_string.length() - 2], card_string[card_string.length() - 1]);
-				std::stringstream card_value_stream;
-				card_value_stream << card_string[card_string.length() - 2];
-				
-				// save the card value as a number
-				unsigned card_value;
-				if (card_value_stream.str() == "K") {
-					card_value = 13;
-				} else if (card_value_stream.str() == "Q") {
-					card_value = 12;
-				} else if (card_value_stream.str() == "J") {
-					card_value = 11;  
-				} else {
-					card_value_stream >> card_value;
-				}
-				// printf("[card_value: %u]", card_value);
-				// std::cout << ", ";
-
-				// declaration for switch
-				std::stringstream hcard;
-				std::string home_card_string;
-				std::stringstream home_card_value_stream;
-				double home_card_value = 0.0;	// default - empty home stack
-				// find the right home stack
-				switch (card_string[card_string.length() - 1])
-				{
-				case 's':
-					// save the home card value as a number
-					if (home_stacks[0].topCard().has_value()) {
-						hcard << home_stacks[0].topCard().value();	
-					} 
-					break;
-				case 'h':
-					// save the home card value as a number
-					if (home_stacks[1].topCard().has_value()) {
-						hcard << home_stacks[0].topCard().value();	
-					} 
-					break;
-				case 'd':
-					// save the home card value as a number
-					if (home_stacks[2].topCard().has_value()) {
-						hcard << home_stacks[0].topCard().value();	
-					} 
-					break;
-				case 'c':
-					// save the home card value as a number
-					if (home_stacks[3].topCard().has_value()) {
-						hcard << home_stacks[0].topCard().value();	
-					} 
-					break;
-				default:
-					break;
-				}
-				// asign the value of the card on work stack
-				home_card_string = hcard.str();
-				if (home_card_string[1] == 'K') {
-					home_card_value = 13.0;
-				} else if (home_card_string[1] == 'Q') {
-					home_card_value = 12.0;
-				} else if (home_card_string[1] == 'J') {
-					home_card_value = 11.0;  
-				} else {
-					home_card_value_stream << home_card_string[1];
-					home_card_value_stream >> home_card_value;
-				}
-				// return: 13 - diff
-				result = 13 - (card_value - home_card_value);
-   	 	}
-		 	// todo move cards back in order if there is no actuall copy 
-			// std::cout << std::endl;
-		}
-
-		// printf("kulo(%.0f), ", result);
-    return result;
-		// return 0;
-}
-*/
-
 typedef std::tuple<double, std::vector<SearchAction>> rated_actions;
 class comparator {
 	public:
@@ -751,7 +389,7 @@ std::vector<SearchAction> AStarSearch::solve(const SearchState &init_state) {
 	}
 	next_values_computed = false;
 
-	std::cout << std::endl << pq_open.size() << std::endl;
+	// std::cout << std::endl << pq_open.size() << std::endl;
 
 	int i = 0;
 	// repeat until the queue is not empty
@@ -763,7 +401,7 @@ std::vector<SearchAction> AStarSearch::solve(const SearchState &init_state) {
 		pq_open.pop();
 		current_actions = std::get<1>(t);
 
-		std::cout << "pq size: " << pq_open.size() << ", current depth: " << current_actions.size() << std::endl;
+		// std::cout << "pq size: " << pq_open.size() << ", current depth: " << current_actions.size() << std::endl;
 
 		// execute actions from the init state
 		SearchState working_state(init_state);
@@ -774,7 +412,8 @@ std::vector<SearchAction> AStarSearch::solve(const SearchState &init_state) {
 			
 		// check if the current state is final
 		if (working_state.isFinal()){
-			std::cout << std::endl << "Real solution returned in depth: " << current_actions.size() << "\nfinal_state:\n" << working_state << std::endl;
+			std::cout << "Real solution returned in depth: " << current_actions.size() << std::endl;
+			// std::cout << "final_state:\n" << working_state << std::endl;
 			return current_actions;
 		}
 		
